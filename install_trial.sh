5d8b33f977bb69c6cc374aff928274d2996a783d
flukadocker <paola.sala@mi.infn.it>
2025-06-29 14:06:52 +0200

    trial

---
+++ install_trial.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define installer script's name and path
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+# Change to scripts directory
+cd $SCRIPTPATH
+
+# Update scripts
+echo "### - Updating installer scripts"
+
+git checkout master
+git pull --force
+
+# Execute installer scripts
+echo "### - Starting installation"
+
+exec ./scripts/install_trial.sh "$@" >&1 | tee install.log
+
+# Change back to original directory
+cd -
+
+echo "### - Installation finished"
